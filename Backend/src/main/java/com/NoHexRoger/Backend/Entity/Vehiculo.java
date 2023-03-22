d3546aec8e7c03ea10cf1874ddcdab4024da59ad
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-03-22 00:59:10 -0400

    add vehiculo properties

--- Backend/src/main/java/com/NoHexRoger/Backend/Entity/Vehiculo.java
+++ Backend/src/main/java/com/NoHexRoger/Backend/Entity/Vehiculo.java
@@ -1,7 +1,25 @@
 package com.NoHexRoger.Backend.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 @Entity(name = "vehiculo")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Vehiculo {
+    @Id
+    private String placa;
+
+    @OneToMany
+    @JoinColumn(name = "tipo_vehiculo_id")
+    private TipoVehiculo tipoVehiculo;
 }
