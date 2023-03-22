7ed5ae621ba9ca166b49a8e6606f0b89eabbd1e2
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-03-22 11:56:27 -0400

    add missing annotations

--- Backend/src/main/java/com/NoHexRoger/Backend/Repository/TipoVehiculoRepository.java
+++ Backend/src/main/java/com/NoHexRoger/Backend/Repository/TipoVehiculoRepository.java
@@ -2,7 +2,9 @@ package com.NoHexRoger.Backend.Repository;
 
 import com.NoHexRoger.Backend.Entity.TipoVehiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TipoVehiculoRepository extends JpaRepository<TipoVehiculo, Long> {
     TipoVehiculo findByTipo(String tipo);
 }
