6704c4125ed6ce51a5a51b2a3c7826a04f0d4491
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-03-25 22:17:44 -0400

    rename field

--- Backend/src/main/java/com/NoHexRoger/Backend/Dto/VehiculoResponse.java
+++ Backend/src/main/java/com/NoHexRoger/Backend/Dto/VehiculoResponse.java
@@ -11,6 +11,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class VehiculoResponse {
     private String placa;
-    private Integer tiempoAcumulado;
+    private int minutosAcumulados;
     private Long tipoVehiculoId;
 }
