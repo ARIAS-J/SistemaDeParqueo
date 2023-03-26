fd38fe0f411cbd71048eb0390ea0931c24741b57
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-03-25 22:18:16 -0400

    stop extending, add missing annotations

--- Backend/src/main/java/com/NoHexRoger/Backend/Dto/VehiculoRequest.java
+++ Backend/src/main/java/com/NoHexRoger/Backend/Dto/VehiculoRequest.java
@@ -1,4 +1,15 @@
 package com.NoHexRoger.Backend.Dto;
 
-public class VehiculoRequest extends VehiculoResponse {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VehiculoRequest {
+    private String placa;
+    private Long tipoVehiculoId;
 }
