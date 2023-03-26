ae43e16d318c619a3e1822bfc56ab209ec34e1d3
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-03-26 18:02:54 -0400

    add initial frontend structure

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EstanciasComponent } from './pages/estancias/estancias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HomeComponent,
+    EstanciasComponent
   ],
   imports: [
     BrowserModule,
