6f74f62091d8725d1d40973ff370c086042f6164
No-Hex <evilheroe1@gmail.com>
2023-03-27 14:04:33 -0400

    Add vehiculo routing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EstanciasComponent } from './pages/estancias/estancias.component';
 import { HomeComponent } from './pages/home/home.component';
+import { VehiculosComponent } from './pages/vehiculos/vehiculos.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'estancias',
     component: EstanciasComponent,
+  },
+  {
+    path: 'vehiculos',
+    component: VehiculosComponent,
   },
 ];
 
