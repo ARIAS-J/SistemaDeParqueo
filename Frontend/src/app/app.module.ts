20adecffd12e13bdee0a16d000e5f51edfa15cce
No-Hex <evilheroe1@gmail.com>
2023-03-27 14:04:49 -0400

    Add vehiculo component to app module

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { EstanciasComponent } from './pages/estancias/estancias.component';
+import { VehiculosComponent } from './pages/vehiculos/vehiculos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
     HomeComponent,
-    EstanciasComponent
+    EstanciasComponent,
+    VehiculosComponent
   ],
   imports: [
     BrowserModule,
