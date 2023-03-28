affe2b377dedffbc594dc05ec598780ba9871f57
No-Hex <evilheroe1@gmail.com>
2023-03-28 11:16:34 -0400

    Added action method getVehiculos to vehiculo component

--- Frontend/src/app/pages/vehiculos/vehiculos.component.ts
+++ Frontend/src/app/pages/vehiculos/vehiculos.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Vehiculo } from 'src/app/shared/vehiculo';
+import { VehiculoService } from './Service/vehiculo.service';
 
 @Component({
   selector: 'app-vehiculos',
   templateUrl: './vehiculos.component.html',
-  styleUrls: ['./vehiculos.component.css']
+  styleUrls: ['./vehiculos.component.css'],
 })
-export class VehiculosComponent {
+export class VehiculosComponent implements OnInit {
+  public vehiculos: Vehiculo[] = [];
 
+  constructor(private vehiculoService: VehiculoService) {}
+
+  ngOnInit(): void {
+    this.vehiculoService
+      .getVehiculos()
+      .subscribe((vehiculos) => (this.vehiculos = vehiculos));
+  }
 }
